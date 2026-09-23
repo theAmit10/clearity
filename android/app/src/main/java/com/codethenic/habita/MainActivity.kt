e52878444ccbdd123233b52b0d6331d4097b8ef2
theAmit10 <prasadak66@gmail.com>
2026-09-23 10:40:55 +0530

    onboarding content updated

--- android/app/src/main/java/com/codethenic/habita/MainActivity.kt
+++ android/app/src/main/java/com/codethenic/habita/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.codethenic.habita
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Swap the launch theme back to the app theme after the splash is shown.
+    setTheme(com.codethenic.habita.R.style.AppTheme)
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
